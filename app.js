061360ca1b5ca7b30a223f7276458cb2c44faf1f
Stacy <stacello19@gmail.com>
2019-09-28 16:21:21 -0400

    finish for now

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.set('port', process.env.PORT || 8005);
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/gif', express.static(path.join(__dirname, 'uploads')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -60,7 +61,7 @@ app.use((err, req, res, next) => {
 });
 
 const server = app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기중');
+  console.log(app.get('port'), ' port listening..');
 });
 
 webSocket(server, app, sessionMiddleware);
