62de95c0632d434cd734d2ab85e483d0b8517f57
Stacy <stacello19@gmail.com>
2019-09-28 16:08:22 -0400

    exchange the msg

--- routes/index.js
+++ routes/index.js
@@ -1,82 +1,100 @@
 const express = require('express');
-const router = express.Router();
+
 const Room = require('../schemas/room');
 const Chat = require('../schemas/chat');
 
-router.get('/', (req, res) => {
-    try{
-        const rooms = await Room.find({});
-        res.render('main', {rooms, title: 'GIF CHAT', error: req.flash('roomError')})
-    }catch (err) {
-        console.error(err);
-        next(err)
-    }
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const rooms = await Room.find({});
+    res.render('main', { rooms, title: 'GIF 채팅방', error: req.flash('roomError') });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 router.get('/room', (req, res) => {
-    res.render('/room', {title: 'GIF CHAT ROOM IS CREATED'})
+  res.render('room', { title: 'GIF 채팅방 생성' });
 });
 
 router.post('/room', async (req, res, next) => {
-    try{
-        const room = new Room({
-            title: req.body.title,
-            max: req.body.max,
-            owner: req.session.color,
-            password: req.body.password
-        });
-        const newRoom = await room.save();
-        const io = req.app.get('io');
-        io.of('/room').emit('newRoom', newRoom);
-        res.redirect(`/room/${newRoom._id}?password=${req.body.password}`)
-    } catch(err) {
-        console.error(err);
-        next(err);
-    }
-})
-
-router.get('/room/:id', async (req, res, next) => {
-    try{
-        const room = await Room.findOne({_id: req.params.id});
-        const io = req.app.get('io');
-        if(!room) {
-            req.flash('roomError', 'Room does not exist..');
-            return res.redirect('/')
-        }
-        if(room.password && room.password !== req.query.password) {
-            req.flash('roomError', 'Password does not match');
-            return res.redirect('/')
-        }
-        const { rooms } = io.of('/chat').adapter;
-        if(rooms && rooms[req.params.id] && room.max < rooms[req.params.id].length) {
-            req.flash('roomError', 'Too many people are in the room');
-            return res.redirect('/');
-        }
-        return res.render('chat', {
-            room,
-            title: room.title,
-            chats: [],
-            user: req.session.color
-        })
-    } catch(err) {
-        console.error(error);
-        return next(error);
-    }
+  try {
+    const room = new Room({
+      title: req.body.title,
+      max: req.body.max,
+      owner: req.session.color,
+      password: req.body.password,
+    });
+    const newRoom = await room.save();
+    const io = req.app.get('io');
+    io.of('/room').emit('newRoom', newRoom);
+    res.redirect(`/room/${newRoom._id}?password=${req.body.password}`);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
-router.delete('/room/:id', async(req, res, next) => {
-    try{
-        await Room.remove({_id: req.params.id});
-        await Chat.remove({room: req.params.id});
-        res.send('ok');
-        setTimeout(() => {
-            req.app.get('io').of('/room').emit('removeRoom', req.params.id);
-        }, 2000);
-    } catch(err) {
-        console.error(err);
-        next(err)
+router.get('/room/:id', async (req, res, next) => {
+  try {
+    const room = await Room.findOne({ _id: req.params.id });
+    const io = req.app.get('io');
+    if (!room) {
+      req.flash('roomError', 'Room does not exist.');
+      return res.redirect('/');
     }
-})
+    if (room.password && room.password !== req.query.password) {
+      req.flash('roomError', 'Wrong password.');
+      return res.redirect('/');
+    }
+    const { rooms } = io.of('/chat').adapter;
+    if (rooms && rooms[req.params.id] && room.max <= rooms[req.params.id].length) {
+      req.flash('roomError', 'Exceeded the capacity.');
+      return res.redirect('/');
+    }
+    const chats = await Chat.find({ room: room._id }).sort('createdAt');
+    return res.render('chat', {
+      room,
+      title: room.title,
+      chats,
+      user: req.session.color,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+router.delete('/room/:id', async (req, res, next) => {
+  try {
+    await Room.remove({ _id: req.params.id });
+    await Chat.remove({ room: req.params.id });
+    res.send('ok');
+    setTimeout(() => {
+      req.app.get('io').of('/room').emit('removeRoom', req.params.id);
+    }, 2000);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/room/:id/chat', async (req, res, next) => {
+  try {
+    const chat = new Chat({
+      room: req.params.id,
+      user: req.session.color,
+      chat: req.body.chat,
+    });
+    await chat.save();
+    req.app.get('io').of('/chat').to(req.params.id).emit('chat', chat);
+    res.send('ok');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 module.exports = router;
-
--- socket.js
+++ socket.js
@@ -2,52 +2,54 @@ const SocketIO = require('socket.io');
 const axios = require('axios');
 
 module.exports = (server, app, sessionMiddleware) => {
-    const io = new SocketIO(server, {path: '/socket.io'});
+  const io = SocketIO(server, { path: '/socket.io' });
 
-    app.set('io', io);
-    const room = io.of('/room');
-    const chat = io.of('/chat');
-    io.use((socket,next) => {
-        sessionMiddleware(socket.request, socket.request.res, next)
-    })
+  app.set('io', io);
+  const room = io.of('/room');
+  const chat = io.of('/chat');
 
-    room.on('connection', (socket) => {
-        console.log('room namespace connected')
-        socket.on('disconnect', () => {
-            console.log('room namespace disconnected')
-        })
+  io.use((socket, next) => {
+    sessionMiddleware(socket.request, socket.request.res, next);
+  });
+
+  room.on('connection', (socket) => {
+    console.log('room namespace connected');
+    socket.on('disconnect', () => {
+      console.log('room namespace disconnected');
     });
-    chat.on('connection', (socket) => {
-        console.log('chat namespace connected');
-        const req=socket.request;
-        const { headers: {referer}} = req;
-        const roomId = referer
-            .split('/')[referer.split('/').length-1]
-            .replace(/\?.+/, '');
-        socket.join(roomId);
-        socket.to(roomId).emit('join', {
-            user: 'system',
-            chat: `${req.session.color} has entered the room`
-        })
-        socket.on('disconnect', () => {
-            console.log('chat namespace disconnected')
-            socket.leave(roomId);
-            const currentRoom = socket.adapter.rooms[roomId];
-            const userCount = currentRoom ? currentRoom.length : 0;
-            if(userCount === 0) {
-                axios.delete(`http://localhost:8005/room/${roomId}`)
-                    .then(() => {
-                        console.log('Room is deleted')
-                    })
-                    .catch((err) => {
-                        console.error(err)
-                    })
-            } else {
-                socket.to(roomId).emit('exit', {
-                    user: 'system',
-                    chat: `${req.session.color} has existed the room`
-                })
-            }
-        })
-    })
-}
+  });
+
+  chat.on('connection', (socket) => {
+    console.log('chat namespace connected');
+    const req = socket.request;
+    const { headers: { referer } } = req;
+    const roomId = referer
+      .split('/')[referer.split('/').length - 1]
+      .replace(/\?.+/, '');
+    socket.join(roomId);
+    socket.to(roomId).emit('join', {
+      user: 'system',
+      chat: `${req.session.color} has entered.`,
+    });
+    socket.on('disconnect', () => {
+      console.log('chat namespace disconnected');
+      socket.leave(roomId);
+      const currentRoom = socket.adapter.rooms[roomId];
+      const userCount = currentRoom ? currentRoom.length : 0;
+      if (userCount === 0) {
+        axios.delete(`http://localhost:8005/room/${roomId}`)
+          .then(() => {
+            console.log('Room is successfully deleted');
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      } else {
+        socket.to(roomId).emit('exit', {
+          user: 'system',
+          chat: `${req.session.color} has left the room.`,
+        });
+      }
+    });
+  });
+};
